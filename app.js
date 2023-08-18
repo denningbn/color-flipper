e18a9d9d6f35d9068ac9121f07181cd473b59608
denningbn <denningbn@appstate.edu>
2023-08-18 16:40:43 +0000

    added logic to change background color and change text in webpage

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const colors = ["Salmon", "PowderBlue", "#00FF7F", "#98FB98", "rgba(111,222,333)"];
+let colors = ["Salmon", "PowderBlue", "#00FF7F", "#98FB98", "rgba(111,222,333)"];
+
+let btn = document.getElementById('btn');
+
+let color = document.querySelector(".color");
+let colorInc = 0;
+btn.addEventListener('click', function(){
+
+	//change background color of body to whatever colorInc is
+	document.body.style.backgroundColor = colors[colorInc];
+
+	//change the text of color to be the new color
+	color.textContent = colors[colorInc];
+
+	colorInc++;
+
+	if (colorInc > 4) {
+		colorInc = 0;
+	}
+})
