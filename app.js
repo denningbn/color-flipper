9f5bea4098bc6496649bde9150a84f2755216061
denningbn <denningbn@appstate.edu>
2023-08-19 10:49:01 +0000

    added css to enhance visual quality of app

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ let colors = ["Salmon", "PowderBlue", "#00FF7F", "#98FB98", "rgba(111,222,333)"]
 let btn = document.getElementById('btn');
 
 let color = document.querySelector(".color");
-let colorInc = 0;
 btn.addEventListener('click', function(){
 
+	let colorNum = getRandomNum();
+
 	//change background color of body to whatever colorInc is
-	document.body.style.backgroundColor = colors[colorInc];
+	document.body.style.backgroundColor = colors[colorNum];
 
 	//change the text of color to be the new color
-	color.textContent = colors[colorInc];
-
-	colorInc++;
-
-	if (colorInc > 4) {
-		colorInc = 0;
-	}
+	color.textContent = colors[colorNum];
 })
+
+function getRandomNum(){
+	return Math.floor(Math.random() * colors.length);
+}
